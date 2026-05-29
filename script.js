f6ac81c30504a18b2ea9ff94fe1e5e61a5b37f0a
yuyuesun <yuyuesun@tencent.com>
2026-05-29 15:04:17 +0800

    退出时保存了一下

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const button = document.querySelector("#helloButton");
 const message = document.querySelector("#message");
+const card = document.querySelector("#aboutCard");
 
 button.addEventListener("click", function () {
   message.textContent = "你点击了按钮！";
+  card.classList.toggle("highlight");
 });
